b0dc70fddf7ef5ca831dd14d43fb08300c3efe44
esaniello <shaniel29samadhan@gmail.com>
2023-07-31 22:53:48 -0300

    feat: :sparkles: u can now specify a custom rate for every employee. It does not take the default clockify rate anymore

--- app.py
+++ app.py
@@ -1,51 +1,54 @@
 import csv
 import datetime
 
+
 # function to check if a date is in the weekend
 def is_weekend(date_str):
     # Convert the date string to a datetime object
     date = datetime.datetime.strptime(date_str, "%d/%m/%Y")
-    
+
     # Check if the weekday is either Saturday (5) or Sunday (6)
     if date.weekday() == 5:
-        return 'Saturday'
+        return "Saturday"
     elif date.weekday() == 6:
-        return 'Sunday'
+        return "Sunday"
     else:
-        return 'Nope'
-    
+        return "Nope"
+
+
 # Read in CSV file exported from Clockify and calc total hours and pay including extra bonus on weekends
-def calcPay(name):
-    with open('data.csv', mode='r') as csv_file:
+def calcPay(name, hourly_rate):
+    with open("data.csv", mode="r") as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
 
         total_hours = 0
         billable_amount = 0
         billable_amount_extra = 0
-        rate = 0
+        rate = hourly_rate
         for row in csv_reader:
             if line_count == 0:
                 print(f'Column names are {", ".join(row)}')
                 line_count += 1
 
-            if row['User'].lower().strip() == name.lower().strip():
-                print(f'Name: {row["User"]}, Email: {row["Email"]}, Date: {row["End Date"]} Duration (h): {row["Duration (h)"]}, Duration (decimal): {row["Duration (decimal)"]}, Billable Rate (USD): {row["Billable Rate (USD)"]}, Billable Amount (USD): {row["Billable Amount (USD)"]}')
-                
-                todays_billable = float(row["Billable Amount (USD)"])
+            if row["User"].lower().strip() == name.lower().strip():
+                print(
+                    f'Name: {row["User"]}, Email: {row["Email"]}, Date: {row["End Date"]} Duration (h): {row["Duration (h)"]}, Duration (decimal): {row["Duration (decimal)"]}, Billable Rate (USD): {row["Billable Rate (USD)"]}, Billable Amount (USD): {row["Billable Amount (USD)"]}'
+                )
+
+                todays_billable = float(row["Duration (decimal)"]) * float(rate)
                 is_weekend_date = is_weekend(row["End Date"])
                 # add 150% on saturdays
-                if is_weekend_date == 'Saturday':
+                if is_weekend_date == "Saturday":
                     todays_billable = todays_billable + (todays_billable / 2)
 
                 # add 200% on sundays
-                if is_weekend_date == 'Sunday':
+                if is_weekend_date == "Sunday":
                     todays_billable = todays_billable * 2
 
                 total_hours += float(row["Duration (decimal)"])
-                billable_amount += float(row["Billable Amount (USD)"])
+                billable_amount += float(row["Duration (decimal)"]) * float(rate)
                 billable_amount_extra += todays_billable
-                rate = float(row["Billable Rate (USD)"])
 
             line_count += 1
 
@@ -53,15 +56,19 @@ def calcPay(name):
         billable_amount = round(billable_amount, 2)
         billable_amount_extra = round(billable_amount_extra, 2)
 
-        print(f'Processed {line_count} lines.')
+        print(f"Processed {line_count} lines.")
         print("=====================================")
         print("Total hours:", total_hours)
         print("Rate (USD):", rate)
         print("Billable amount (USD):", billable_amount)
-        print("Billable amount extra (USD):", billable_amount_extra)
+        print("Billable amount extra (weekends) (USD):", billable_amount_extra)
         print("=====================================")
 
+
 # call function
-input_name = input("Please enter the name of the employee. (Make sure to use name in Clockify) \n")
-calcPay(input_name)
+input_name = input(
+    "Please enter the name of the employee. (Make sure to use name in Clockify) \n"
+)
+employee_rate = input("Please enter the hourly rate of this employee \n")
+calcPay(input_name, employee_rate)
 print("If the amount is 0 then please verify if u have entered the name correctly")
